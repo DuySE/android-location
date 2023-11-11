00bce2de7cc69f76e730420195a776142bdc3a1e
DuySE <duynse@gmail.com>
2023-11-10 22:57:09 -0800

    Fix crashing error

--- app/src/main/java/com/example/geocoderdemo/MainActivity.java
+++ app/src/main/java/com/example/geocoderdemo/MainActivity.java
@@ -9,6 +9,9 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,9 +26,9 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity implements LocationListener {
     private LocationManager locationManager;
     private TextView txtViewLocation;
-    private Location gpsLocation;
+    private Location userLocation;
 
-    private final int LOCATION_REQUEST_CODE = 1;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +36,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_main);
         txtViewLocation = findViewById(R.id.txtViewLocation);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        geocoder = new Geocoder(this, Locale.getDefault());
         askPermission();
-        calculateDistance();
+        userLocation = new Location(LocationManager.GPS_PROVIDER);
+        Button button = findViewById(R.id.btnGetDistance);
+        button.setOnClickListener(view -> calculateDistance());
     }
 
     private void askPermission() {
@@ -43,25 +49,27 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 ContextCompat.checkSelfPermission(this,
                         Manifest.permission.ACCESS_COARSE_LOCATION) !=
                         PackageManager.PERMISSION_GRANTED) {
+            int LOCATION_REQUEST_CODE = 1;
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
                     LOCATION_REQUEST_CODE);
         } else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
-        gpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
     }
 
     private void calculateDistance() {
-        String locationName = "330 E Columbia St, New Westminster, BC";
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        String locationName = "330 E Columbia St, New Westminster, BC, Canada";
         try {
             List<Address> addressList = geocoder.getFromLocationName(locationName, 1);
             double endLatitude = addressList.get(0).getLatitude();
             double endLongitude = addressList.get(0).getLongitude();
             float[] results = new float[1];
-            Location.distanceBetween(gpsLocation.getLatitude(), gpsLocation.getLongitude(), endLatitude, endLongitude, results);
-            txtViewLocation.setText(results[0] + "");
+            Location.distanceBetween(userLocation.getLatitude(), userLocation.getLongitude(), endLatitude, endLongitude, results);
+            String result = getString(R.string.distance, results[0]);
+            Log.d("AAA", result);
+            txtViewLocation.setGravity(View.TEXT_ALIGNMENT_CENTER);
+            txtViewLocation.setText(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -69,5 +77,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
+        userLocation.setLatitude(location.getLatitude());
+        userLocation.setLongitude(location.getLongitude());
     }
 }
